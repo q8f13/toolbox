91961d7b0dde0e60e8503ea74403642fe5c82d85
q8f13 <dustfall@gmail.com>
2016-09-14 02:31:43 +0800

    basic utility static class

---
+++ qfUtility.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// static utility functions
+/// </summary>
+
+public class qfUtility {
+
+	public static T GetComponentByPath<T>(Transform parent, string path) where T : Component
+	{
+		T result = null;
+	
+		Transform target = parent.FindChild(path);
+		if (target == null)
+		{
+			Debug.LogError(string.Format("cannot get child by path: {0}", path));
+			return result;
+		}
+
+		result = target.GetComponent<T>();
+
+		return result;
+	}
+}
