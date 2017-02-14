b93d98d654190860975727d31b2ae5f22c041f20
q8f13 <dustfall@gmail.com>
2017-02-15 00:57:45 +0800

    collider消息传递类的微调

--- qfDetectionMessenger.cs
+++ qfDetectionMessenger.cs
@@ -1,23 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(Collider))]
+/// <summary>
+/// 将collider检测的结果通过delegate传出
+/// </summary>
 public class qfDetectionMessenger : MonoBehaviour
 {
 	public System.Action<Collider> OnTriggerEnterAction;
 	public System.Action<Collider> OnTriggerExitAction;
 	public System.Action<Collider> OnTriggerStayAction;
 
+	public System.Action<Collider2D> OnTriggerEnter2DAction;
+	public System.Action<Collider2D> OnTriggerExit2DAction;
+	public System.Action<Collider2D> OnTriggerStay2DAction;
+
 	public System.Action<Collision> OnCollisionEnterAction;
 	public System.Action<Collision> OnCollisionExitAction;
 	public System.Action<Collision> OnCollisionStayAction;
 
-	private Collider _collider;
-	public Collider Collider { get { return _collider; } }
-
 	void Start()
 	{
-		_collider = GetComponent<Collider>();
+//		_collider2D = GetComponent<Collider>();
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (OnTriggerEnter2DAction != null)
+			OnTriggerEnter2DAction(collision);
+	}
+
+	void OnTriggerExit2D(Collider2D collision)
+	{
+		if (OnTriggerExit2DAction != null)
+			OnTriggerExit2DAction(collision);
+	}
+
+	void OnTriggerStay2D(Collider2D collision)
+	{
+		if (OnTriggerStay2DAction != null)
+			OnTriggerStay2DAction(collision);
 	}
 
 	void OnTriggerEnter(Collider col)
