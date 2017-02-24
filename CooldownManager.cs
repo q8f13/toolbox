49a954b4dd45ac11c451044a68de4c9e4d9b28cb
kxb <dustfall@gmail.com>
2017-02-24 13:20:19 +0800

    add cooldown manager

---
+++ CooldownManager.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// cooldown event manager
+/// track item by key
+/// dispatch 'complete' via delegate
+/// </summary>
+public class CooldownManager : MonoBehaviour
+{
+	private static CooldownManager _instance = null;
+	public static CooldownManager Instance { get { return _instance; } }
+	
+	private Dictionary<string, CooldownItem> _cooldownPool = new Dictionary<string, CooldownItem>();
+
+	#region UnityEvents
+	void Awake()
+	{
+		_instance = this;
+	}
+
+	void OnDestroy()
+	{
+		foreach (KeyValuePair<string, CooldownItem> item in _cooldownPool)
+		{
+			item.Value.Dispose();
+			_cooldownPool.Clear();
+		}
+	}
+
+	void Update()
+	{
+		foreach (KeyValuePair<string, CooldownItem> item in _cooldownPool)
+		{
+			item.Value.Update(Time.deltaTime);
+		}
+	}
+
+	#endregion
+
+	public bool TrackCooldown(string key, float time, CooldownItem.OnCooldownReadyEvent readyHandler)
+	{
+		if (!_cooldownPool.ContainsKey(key))
+		{
+			CooldownItem tracker = new CooldownItem(key);
+			tracker.OnCooldownReadyTrigger = readyHandler;
+			_cooldownPool.Add(key, tracker);
+		}
+
+		bool success = _cooldownPool[key].TryStartCounting(time);
+
+		if(!success)
+			Debug.LogError(string.Format("fail to track cooldown of key {0}", key));
+
+		return success;
+	}
+
+	public bool IsReady(string key)
+	{
+		if (!_cooldownPool.ContainsKey(key))
+		{
+			Debug.LogError(string.Format("no tracking item key mapped as {0}", key));
+			return false;
+		}
+
+		return _cooldownPool[key].IsReady();
+	}
+
+	public float GetCooldownTimeLeft(string key)
+	{
+		if (!_cooldownPool.ContainsKey(key))
+		{
+			Debug.LogError(string.Format("no tracking item key mapped as {0}", key));
+			return -1.0f;
+		}
+
+		return _cooldownPool[key].Timer;
+	}
+}
+
+/// <summary>
+/// cooldown item data structure
+/// container for cooldown tracking
+/// </summary>
+public class CooldownItem :IDisposable
+{
+	public string Key;
+	public float Timer = 0.0f;
+
+	public delegate void OnCooldownReadyEvent(string key);
+	public OnCooldownReadyEvent OnCooldownReadyTrigger;
+
+	public CooldownItem(string key)
+	{
+		Key = key;
+	}
+
+	/// <summary>
+	/// start a item cooldown tracking by key for the constructor
+	/// </summary>
+	/// <param name="time"></param>
+	/// <returns>is tracking succeed</returns>
+	public bool TryStartCounting(float time)
+	{
+		if (!IsReady())
+			return false;
+
+		Timer = time;
+		return true;
+	}
+
+	public bool IsReady()
+	{
+		return Timer <= 0.0f;
+	}
+
+	/// <summary>
+	/// refresh time counting
+	/// </summary>
+	/// <param name="deltaTime"></param>
+	public void Update(float deltaTime)
+	{
+		if (Timer > 0.0f)
+		{
+			Timer -= deltaTime;
+			if (Timer <= 0.0f && OnCooldownReadyTrigger != null)
+				OnCooldownReadyTrigger(Key);
+		}
+	}
+
+	public void Dispose()
+	{
+		OnCooldownReadyTrigger = null;
+	}
+}
---
+++ CooldownTester.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Test case for CooldownManager
+/// </summary>
+public class CooldownTester : MonoBehaviour
+{
+	private static Dictionary<string, int> PRESET_ITEMS = new Dictionary<string, int>()
+	{
+		{"aaa", 15},
+		{"bbb", 6},
+		{"ccc", 9},
+		{"ddd", 2},
+		{"eee", 4},
+		{"fff", 5}
+	};
+
+	private List<string> _trackingKeys = new List<string>();
+	private string _nextRandomizedKey = null;
+
+	private static string[] PRESET_KEYS = null;
+
+	private CooldownManager _manager;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_manager = gameObject.AddComponent<CooldownManager>();
+		
+		PRESET_KEYS = PRESET_ITEMS.Keys.ToArray();
+		_nextRandomizedKey = GetRandomKey();
+	}
+
+	string GetRandomKey()
+	{
+		int rnd = Random.Range(0, PRESET_ITEMS.Count);
+		return PRESET_KEYS[rnd];
+	}
+	
+	void OnGUI()
+	{
+		if (GUI.Button(new Rect(0, 0, 400, 80), string.Format("add {0}", _nextRandomizedKey)))
+		{
+			if (_manager.TrackCooldown(_nextRandomizedKey, PRESET_ITEMS[_nextRandomizedKey], CooldownCompleteHint))
+			{
+				_trackingKeys.Add(_nextRandomizedKey);
+			}
+			_nextRandomizedKey = GetRandomKey();
+		}
+
+		for (int i = 0; i < _trackingKeys.Count; i++)
+		{
+			GUI.Label(new Rect(0, 200 + 40*i, 400, 90), string.Format("{0} : {1:f1}"
+				, _trackingKeys[i]
+				, _manager.GetCooldownTimeLeft(_trackingKeys[i])));
+		}
+	}
+
+	void CooldownCompleteHint(string key)
+	{
+		Debug.Log(string.Format("cooldown of =========>{0}<======== is ready!", key));
+		_trackingKeys.Remove(key);
+	}
+}
